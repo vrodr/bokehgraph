8f42b79a915ebf33816cabaefc0a90416a56fe7a
Rustam A <40663135+vrodr@users.noreply.github.com>
2018-10-28 23:46:37 +0400

    Work slowly on csv

--- bokehgraph.py
+++ bokehgraph.py
@@ -1,25 +1,38 @@
 import numpy as np
 import csv
 from bokeh.plotting import figure, output_file, show
+import random
+from concurrent.futures import ThreadPoolExecutor
+
+def random_color():
+    r = lambda: random.randint(0,255)
+    return ('#%02X%02X%02X' % (r(),r(),r()))
+
+
 
 # output to static HTML file (with CDN resources)
-output_file("color_scatter.html", title="color_scatter.py example", mode="cdn")
+output_file("circles.html", title="circles", mode="cdn")
 
 TOOLS = "crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
 
 # create a new plot with the tools above, and explicit ranges
 p = figure(tools=TOOLS, x_range=(-100, 100), y_range=(-100, 100))
-list_of_tems = []
+
+dict_of_tems = {}
+list_of_colors = []
+
 # prepare some data
-with open('mini.csv') as f:
+with open('data/mini.csv') as f:
     reader = csv.reader(f)
     for row in reader:
-        if row[4] not in list_of_tems:
-            list_of_tems.append(row[4])
-    for tem in list_of_tems:
-        tem_collor =
-    for row in reader:
-        p.circle(float(row[1]), float(row[2]), fill_color=color, fill_alpha=0.6, line_color=None)
+        if row[4] not in dict_of_tems:
+            color = random_color()
+            while color in list_of_colors:
+                color = random_color()
+            list_of_colors.append(color)
+            dict_of_tems[row[4]] = color
+        p.circle(float(row[1]), float(row[2]), fill_color=dict_of_tems[row[4]], fill_alpha=0.6, line_color=None)
+
 
 
 
