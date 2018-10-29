fd3716413ba419811c7c65301acb9dcc97149aec
Асадуллин Рустам Раилевич <16680894@sigma.sbrf.ru>
2018-10-29 11:45:56 +0400

    Main functionality works

--- bokehgraph.py
+++ bokehgraph.py
@@ -1,39 +1,39 @@
 import pandas as pd
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, ColumnDataSource
 import random
-from concurrent.futures import ThreadPoolExecutor
 
+
+# random color for themes
 def random_color():
-    r = lambda: random.randint(0,255)
-    return ('#%02X%02X%02X' % (r(),r(),r()))
+    r = lambda: random.randint(0, 255)
+    return '#%02X%02X%02X' % (r(), r(), r())
 
+TOOLS = "crosshair, pan, box_zoom, reset, hover"
+TOOLTIPS = [("index", "$index"),
+            ("(x, y)", "($x, $y)"),
+            ("desc", "@Message")]
 
-
-# output to static HTML file (with CDN resources)
 output_file("circles.html", title="circles", mode="cdn")
+p = figure(tools=TOOLS, x_range=(-40, 40), y_range=(-40, 40), tooltips=TOOLTIPS)
 
-TOOLS = "crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
-
-# create a new plot with the tools above, and explicit ranges
-p = figure(tools=TOOLS, x_range=(-100, 100), y_range=(-100, 100))
-
-dict_of_tems = {}
-list_of_colors = []
-i = 0
-
-# prepare some data
+# prepare data
 df = pd.read_csv('data/export.csv', sep=',')
-for row in df.iterrows():
-    if row[1]['Result Chat'] not in dict_of_tems:
+dict_of_themes = {}
+list_of_colors = []
+for ind, row in df.iterrows():
+    if row['Result Chat'] not in dict_of_themes:
         color = random_color()
         while color in list_of_colors:
             color = random_color()
         list_of_colors.append(color)
-        dict_of_tems[row[1]['Result Chat']] = color
-    p.circle(float(row[1]['x']), float(row[1]['y']), fill_color=dict_of_tems[row[1]['Result Chat']], fill_alpha=0.6, line_color=None)
-    i += 1
-    print(i)
+        dict_of_themes[row['Result Chat']] = color
+df['color'] = df['Result Chat']
+df = df.replace({'color': dict_of_themes})
+
+source = ColumnDataSource(df)
+
+#build dots
+p.circle('x', 'y', fill_color='color', line_color='color', source=source)
 
 # show the results
 show(p)
-
